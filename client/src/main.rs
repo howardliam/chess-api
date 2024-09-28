8fb1cbc7857071fd7a3934b1d0fa0ea44b7b3664
Liam Howard <lima.mail725@gmail.com>
2024-09-28 16:29:41 +1000

    Created basic client and server.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,101 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    collections::VecDeque,
+    ops::ControlFlow,
+    sync::{Arc, Mutex},
+    time::Duration,
+};
+
+use futures::{SinkExt, StreamExt};
+use tokio::time;
+use tokio_tungstenite::{connect_async, tungstenite::Message};
+use tracing::{error, info};
+
+const URI: &str = "ws://127.0.0.1:3000/ws";
+
+type MessageQueue = Arc<Mutex<VecDeque<String>>>;
+
+#[tokio::main]
+async fn main() {
+    let queue: MessageQueue = Arc::new(Mutex::new(VecDeque::new()));
+
+    let mut interval = time::interval(Duration::from_secs(1));
+
+    tracing_subscriber::fmt().init();
+
+    let ws_stream = match connect_async(URI).await {
+        Ok((stream, _res)) => {
+            info!("Handshake completed");
+            stream
+        }
+        Err(err) => {
+            error!("Websocket handshake failed with {err}!");
+            return;
+        }
+    };
+
+    let (mut sender, mut receiver) = ws_stream.split();
+
+    let send_queue = queue.clone();
+    let mut send_task = tokio::spawn(async move {
+        loop {
+            interval.tick().await;
+
+            let next_msg = {
+                let mut queue = send_queue.lock().unwrap();
+                queue.pop_front()
+            };
+
+            if let Some(next_msg) = next_msg {
+                sender.send(Message::Text(next_msg)).await;
+            }
+        }
+    });
+
+    let recv_queue = queue.clone();
+    let mut recv_task = tokio::spawn(async move {
+        while let Some(Ok(msg)) = receiver.next().await {
+            if process_message(&msg).is_break() {
+                break;
+            }
+
+            if let Message::Text(t) = msg {
+                let res = match t.as_str() {
+                    "uci" => "uciok",
+                    _ => "unknown command",
+                };
+
+                recv_queue.lock().unwrap().push_back(res.to_owned());
+            }
+        }
+    });
+
+    tokio::select! {
+        _ = (&mut send_task) => {
+            recv_task.abort();
+        },
+        _ = (&mut recv_task) => {
+            send_task.abort();
+        }
+    }
+}
+
+fn process_message(message: &Message) -> ControlFlow<(), ()> {
+    match message {
+        Message::Text(t) => {
+            info!("Received text `{t}`");
+        }
+        Message::Close(cf) => {
+            if let Some(cf) = cf {
+                info!(
+                    "Received close with code {} and reason {}",
+                    cf.code, cf.reason
+                );
+            }
+
+            return ControlFlow::Break(());
+        }
+        _ => {}
+    }
+
+    ControlFlow::Continue(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,87 @@
-fn main() {
-    println!("Hello, world!");
+use std::{net::SocketAddr, ops::ControlFlow};
+
+use axum::{
+    extract::{
+        ws::{Message, WebSocket},
+        ConnectInfo, WebSocketUpgrade,
+    },
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
+use futures::{SinkExt, StreamExt};
+use tokio::net::TcpListener;
+use tracing::{error, info};
+
+const URL: &str = "127.0.0.1:3000";
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt().init();
+
+    let listener = TcpListener::bind(URL).await?;
+    let app = Router::new().route("/ws", get(ws_handler));
+
+    axum::serve(
+        listener,
+        app.into_make_service_with_connect_info::<SocketAddr>(),
+    )
+    .await?;
+
+    Ok(())
+}
+
+async fn ws_handler(
+    ws: WebSocketUpgrade,
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
+) -> impl IntoResponse {
+    info!("{addr} connected");
+    ws.on_upgrade(move |socket| handle_socket(socket, addr))
+}
+
+async fn handle_socket(socket: WebSocket, addr: SocketAddr) {
+    let (mut sender, mut receiver) = socket.split();
+
+    let mut send_task = tokio::spawn(async move {
+        sender.send(Message::Text("uci".into())).await;
+        loop {}
+    });
+
+    let mut recv_task = tokio::spawn(async move {
+        while let Some(Ok(msg)) = receiver.next().await {
+            if process_message(msg).is_break() {
+                break;
+            }
+        }
+    });
+
+    tokio::select! {
+        _ = (&mut send_task) => {
+            recv_task.abort();
+        },
+        _ = (&mut recv_task) => {
+            send_task.abort();
+        }
+    }
+}
+
+fn process_message(message: Message) -> ControlFlow<(), ()> {
+    match message {
+        Message::Text(t) => {
+            info!("Received text `{t}`");
+        }
+        Message::Close(cf) => {
+            if let Some(cf) = cf {
+                info!(
+                    "Received close with code {} and reason {}",
+                    cf.code, cf.reason
+                );
+            }
+
+            return ControlFlow::Break(());
+        }
+        _ => {}
+    }
+
+    ControlFlow::Continue(())
 }
